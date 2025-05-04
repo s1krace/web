44efbe1d10c20afba25b16e9ecfa01aaa084eece
racecar <Raderaka@outlook.com>
2025-05-04 17:07:20 +1000

    Delete ascii-init.js

--- ascii-init.js
+++
@@ -1,8 +0,0 @@
-import { run } from '/src/run.js';
-import * as program from '/ascii.js';
-run(program, { element: document.querySelector('pre') }).then(function (e) {
-    console.log(e)
-}).catch(function (e) {
-    console.warn(e.message)
-    console.log(e.error)
-});
